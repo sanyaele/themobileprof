558f4517abe54c0b04ab7982d1e2a45150e20332
samuel <samuelanyaele@gmail.com>
2018-11-26 17:18:34 +0100

    Initial commit

---
+++ includes/db.php
@@ -0,0 +1,4 @@
+<?php
+	$link = mysqli_connect ("sanyaele03621.ipagemysql.com", "seonigeria", "tummy", "seonigeria") or die ("Connection Error: " . mysqli_connect_error());
+	//$link = mysqli_connect ("localhost", "root", "tummy", "seonigeria") or die ("Connection Error: " . mysqli_connect_error());
+?>
